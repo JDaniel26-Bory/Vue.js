9336b8e8d9e17f754c3b7b83a07bf665c34ec0d4
JDaniel26-Bory <juanrestrepo246@gmail.com>
2023-07-05 16:19:24 -0500

    update of file

---
+++ IntroduccionJS/js/17.js
@@ -0,0 +1,10 @@
+// Condicionales
+
+const disponible = 100
+const retirar = 500
+
+if( disponible < retirar) {
+    console.log('You can retirar')
+} else {
+    console.log('you can not retirar esa cantidad')
+}
---
+++ IntroduccionJS/js/18.js
@@ -0,0 +1,10 @@
+// Operador de comparacion
+const number1 = 20
+const number2 = "20"
+
+if(number1 ===  number2) {
+    console.log('Si son iguales')
+} else {
+    console.log('no, no son iguales')
+}
+
